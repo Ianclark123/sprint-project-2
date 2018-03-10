4a9ea753bab2f552c821128c9da8f8b7e29066bd
Ianclark123 <37228592+Ianclark123@users.noreply.github.com>
2018-03-10 14:10:56 -0800

    sprite project 2 final
    press the mouse and the image will change and there will be a message.

--- Sprite project 2 FINAL.py
+++ Sprite project 2 FINAL.py
@@ -6,7 +6,7 @@ window = pygame.display.set_mode([1018, 573])
 button_1 = sprite_public.Sprite("YesButton.jpg",110,49,window)
 button_2 = sprite_public.Sprite("NoButton.jpg",110,49,window)
 ball = sprite_public.Sprite("TennisBall.jpg", 110, 49,window)
-
+c = pygame.time.Clock()
 x = random.randint(0, 1018)
 y = random.randint(0, 573)
 window.fill((255, 0, 0))
